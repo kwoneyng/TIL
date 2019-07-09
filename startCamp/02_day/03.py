1180b09dffb18729dbd6eae9a6d5a68ea7b2e43a
kwoneyng <52685217+kwoneyng@users.noreply.github.com>
2019-07-09 14:30:18 +0900

    Delete 03.py

--- startCamp/02_day/03.py
+++
@@ -1,8 +0,0 @@
-test_num = int(input())
-test_case = []
-month = []
-for i in range(0, test_num)	:
-    test_case.append(input())
-    month.append(test_case[i][4:6])
-print(test_case)
-print(month)
