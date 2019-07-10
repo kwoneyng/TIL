3834b1f432d5cde2b0d26f9ca015e04269cc0f94
kwoneyng <nan308@naver.com>
2019-07-10 17:09:56 +0900

    Final

--- startCamp/03_day/flask/intro/app.py
+++ startCamp/03_day/flask/intro/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 import datetime
 import hello
 import random
@@ -7,11 +7,13 @@ app = Flask(__name__)
 
 @app.route('/') #데코레이터 @가 붙어있음 endpoint
 def hello(): #hello 함수 return값 = Hello PARK!
-    return 'Hello PARK!'
+    return render_template('index.html')
+
 
 @app.route('/ssafy')
 def ssafy():
     return 'Hello SSAFY'
+
 
 @app.route('/dday')
 def dday():
@@ -20,9 +22,11 @@ def dday():
     td = b_day - today
     return f'{td.days} 일 남았습니다.'
 
+
 @app.route('/html')
 def html():
     return '<h1>This is HTML h1 tag!</h1>'
+
 
 @app.route('/html_lines')
 def html_lines(): 
@@ -34,22 +38,28 @@ def html_lines():
     </ul>
     '''
 
+
 @app.route('/greeting/IU')
 def greeting_IU():
     return '반갑습니다 IU님'
+
 
 @app.route('/greeting/zionT')
 def greeting_zionT():
     return '반갑습니다 zionT님'
     
+
 #Variable Routing
 @app.route('/greeting/<name>')
 def greeting(name):
-    return f'반갑습니다! {name} 님!'
+    return render_template('greeting.html', html_name=name)
+
 
 @app.route('/cube/<int:num>')
 def cube(num):
-    return f'{num}의 3 제곱은 {num**3} 입니다.'
+    result = num ** 3
+    return render_template('cube.html', html_num=num, html_result=result)
+
 
 #실습
 @app.route('/lunch/<int:people>')
@@ -58,6 +68,11 @@ def lunch(people):
     sel = random.sample(menu, people)
     return str(sel)
 
+
+@app.route('/movie')
+def movie():
+    movies = ['스파이더맨', '엔드게임', '기생충', '알라딘']
+    return render_template('movie.html', movies=movies)
 #파이썬 파일들은 모두 모듈이다. 따라서 import hello -> print(hello.message)
 if __name__ == '__main__' : #name이라는 변수에 main이 담긴다
     app.run(debug=True)
