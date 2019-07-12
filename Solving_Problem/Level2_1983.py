2180398bb907cb50d897d40cde710c265c2eee47
kwoneyng <nan308@naver.com>
2019-07-12 18:01:06 +0900

    Homework

---
+++ Solving_Problem/Level2_1983.py
@@ -0,0 +1,19 @@
+test_num = int(input())
+for i in range(test_num):
+    quest = list(map(int, input().split()))
+    Hm_Stud = quest[0]
+    Wh_Stud = quest[1]
+    Score_set = []
+    total_sc = []
+    grade_set = []
+    for j in range(Hm_Stud):
+        Score_set = list(map(int, input().split()))
+        total_sc.append(round(0.35*Score_set[0]+0.45*Score_set[1]+0.2*Score_set[2],2))
+    sort_total_sc = sorted(total_sc)
+    for j in total_sc:
+        g = 1
+        if 
+        grade_set.append
+        
+        
+
