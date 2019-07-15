9f809f70c865ba9a8e8d6c8ac15a1ff5cad2cf82
kwoneyng <nan308@naver.com>
2019-07-15 14:27:26 +0900

    solve level2

--- Solving_Problem/Level2_1983.py
+++ Solving_Problem/Level2_1983.py
@@ -1,19 +1,45 @@
+import operator
+
 test_num = int(input())
 for i in range(test_num):
-    quest = list(map(int, input().split()))
-    Hm_Stud = quest[0]
-    Wh_Stud = quest[1]
-    Score_set = []
-    total_sc = []
-    grade_set = []
-    for j in range(Hm_Stud):
-        Score_set = list(map(int, input().split()))
-        total_sc.append(round(0.35*Score_set[0]+0.45*Score_set[1]+0.2*Score_set[2],2))
-    sort_total_sc = sorted(total_sc)
-    for j in total_sc:
-        g = 1
-        if 
-        grade_set.append
-        
+    a = list(map(int, input().split()))
+    pp = a[0]
+    which = a[1]
+    score_set = []
+    dic_stu = {}
+    dic_rank = []
+    result = ''
+    for j in range(pp):
+        score_set = list(map(int, input().split()))
+        dic_stu[j] = round(0.35*score_set[0]+0.45*score_set[1]+0.2*score_set[2],2)
+    sorted_dic = sorted(dic_stu.items(), key=operator.itemgetter(1),reverse=True)
+    for j in range(len(sorted_dic)):
+        if j < len(sorted_dic)/10 :
+            dic_rank.append([sorted_dic[j][0]+1, 'A+'])
+        elif j < len(sorted_dic)*2/10:
+            dic_rank.append([sorted_dic[j][0]+1, 'A0'])
+        elif j < len(sorted_dic)*3/10:
+            dic_rank.append([sorted_dic[j][0]+1, 'A-'])
+        elif j < len(sorted_dic)*4/10:
+            dic_rank.append([sorted_dic[j][0]+1,'B+'])
+        elif j < len(sorted_dic)*5/10:
+            dic_rank.append([sorted_dic[j][0]+1,'B0'])
+        elif j < len(sorted_dic)*6/10:
+            dic_rank.append([sorted_dic[j][0]+1,'B-'])
+        elif j < len(sorted_dic)*7/10:
+            dic_rank.append([sorted_dic[j][0]+1,'C+'])
+        elif j < len(sorted_dic)*8/10:
+            dic_rank.append([sorted_dic[j][0]+1,'C0'])
+        elif j < len(sorted_dic)*9/10:
+            dic_rank.append([sorted_dic[j][0]+1,'C-'])
+        else :
+            dic_rank.append([sorted_dic[j][0]+1,'D0'])
+    for k in range(len(dic_rank)):
+        if dic_rank[k][0] == which :
+            result = dic_rank[k][1]
+
+    print(f'#{i+1} {result}')
+
         
 
+# python Level2_1983.py
---
+++ Solving_Problem/sol.py
@@ -0,0 +1,5 @@
+dic = {}
+sy = [3,7,5,3,1]
+for i in range(5):
+    dic[i] = sy[i]
+print(dic)
