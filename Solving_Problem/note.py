cd8ddccdedc179e00fb7264ad158dd3ecce0db69
kwoneyng <nan308@naver.com>
2019-07-26 17:02:07 +0900

    busy now I'll change my seat

--- Solving_Problem/note.py
+++ Solving_Problem/note.py
@@ -1,5 +1,11 @@
-for T in range(10):
-    t_no = int(input())
-    var_wd = input()
-    a = list(map(str, input().split(var_wd)))
-    print(f'#{T+1} {len(a)-1}')
+t_s = list(map(str, [11*i for i in range(1,10)]))
+t_s.append('00')
+
+s = '1238099084'
+# for i in t_s:
+#     if i in s:
+#         s.replace(i,'')
+#     else :
+#         print(s)
+s.replace('99', '0',1)
+print(s)
