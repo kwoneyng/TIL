73809ed837ca2ae7c0b7a3f410cffc9bf855bbd0
박권응 <nan308@naver.com>
2019-08-19 12:25:32 +0900

    Solve Problem

--- Solving_Problem/note.py
+++ Solving_Problem/note.py
@@ -1,24 +1,17 @@
+def move(ls, start, cnt=0):
+        for i in range(len(ls)):
+            if ls[start][1] == ls[i][0]:
+                if move(ls, ls[i] cnt+1) == 1:
+                    return 1
+        return 1
+
 for T in range(1):
-    v, e = list(map(int, input().split()))
-    ls_e = []
-    N = list(map(int, input().split()))
-    print(v, e, N)
-    al = set(N)
-    while len(N) > 0 :
+    v, e = map(int, input().split())
+    st = input().split()
+    tar = []
+    while len(st) > 0: 
         su = []
         for i in range(2):
-            su.append(N.pop(0))
-        ls_e.append(su)
-    print(f'ls_e={ls_e}')  # 순서쌍
-    al = list(al)
-    ls = [set() for i in range(len(al))]
-    # [[]]*len(al)
-    print(ls)  # 뒤에 무엇이 나올지 저장하는 공간
-    print(al)  # 포함하고 있는 모든 숫자
-    for i in ls_e:
-        ls[al.index(i[0])].add(i[1])
-    for j in range(1, len(ls)+1):  # 뒤에 순서에 있는거 차근차근 넣어주는 과정
-
-    print(ls)
-        
-    
+            su.append(st.pop(0))
+        tar.append(su)
+    print(tar)
