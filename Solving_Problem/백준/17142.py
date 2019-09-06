8795e31483eaba0ae0b586440bbb2ba08bab7790
박권응 <nan308@naver.com>
2019-09-06 18:00:47 +0900

    solve

--- Solving_Problem/백준/17142.py
+++ Solving_Problem/백준/17142.py
@@ -1,5 +1,5 @@
 from pprint import pprint
-
+near = [(-1,0), (0,1), (1,0), (0,-1)]
 def su_set(i, ls):
     if len(ls) == m:
         ch_set.append(ls)
@@ -21,4 +21,24 @@ for x in range(n):
             virus.append([x,y])
 for i in range(len(virus)):
     su_set(i, [virus[i]])
+for i in ch_set:
+    q = []
+    n_bd = [i[:] for i in bd]
+    vis = [[0]*n for i in range(n)]
+    for j in i:
+        q.append([0,j])
+    while q:
+        time, xy = q.pop(0)
+        x, y = xy
+        for dx, dy in near:
+            xi = x+dx
+            yi = y+dy
+            xyi = [xi, yi]
+            if 0 <= xi < n and 0 <= yi < n:
+                if bd[xi][yi] == 0:
+                    bd[xi][yi] = 2
+                    q.append([time+1, xyi])
+    
+
+
 pprint(ch_set)
