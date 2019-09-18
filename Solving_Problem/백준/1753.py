90539b1a5344647099daa95c94e21fc6b0f62781
kwoneyng <nan308@naver.com>
2019-09-18 19:59:34 +0900

    solve

--- Solving_Problem/백준/1753.py
+++ Solving_Problem/백준/1753.py
@@ -1,3 +1,19 @@
-v,e = map(int, input().split())
+V,E = map(int, input().split())
 start = int(input())
-ln_set = [list(map(int, input().split())) for i in range(e)]
+nxt_ls = {i : {} for i in range(1,V+1)}
+costs = {i : 'inf' for i in range(1, V+1)}
+costs[start] = 0
+q = [(start,0)]
+for i in range(E):
+    u,v,w = list(map(int, input().split()))
+    nxt_ls[u][v] = w
+while q:
+    chk, cost = q.pop()
+    for nxt, nxt_cost in nxt_ls[chk].items(): # 다음에 갈 애들
+        q.append([nxt, cost+nxt_cost])
+        if costs[nxt] == 'inf':
+            costs[nxt] = nxt_cost+cost
+        elif costs[nxt] > nxt_cost+cost:
+            costs[nxt] = nxt_cost+cost
+for i in range(1,V+1):
+    print(costs[i])
