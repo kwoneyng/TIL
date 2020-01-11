0963c32f6d9bc386793394f76a3532f9c83e6226
unknown <nan308@naver.com>
2020-01-11 16:59:18 +0900

    solve

--- 2020/01월/11/알고리즘/프로그래머스_자물쇠와 열쇠.py
+++ 2020/01월/11/알고리즘/프로그래머스_자물쇠와 열쇠.py
@@ -1,19 +1,18 @@
-near = [[0,1], [1,0]]
 def rotate(key):
-    temp = [i[:] for i in key]
+    lis = []
+    for i in range(len(key)):
+        li = []
+        for j in range(len(key)):
+            li.append(key[len(key)-j-1][i])
+        lis.append(li)
+    return lis
 
+def insert(bd,key,lock,cnt,x,y):
+    rs = 0
+    m = len(key)
+    n = len(lock)
     for i in range(len(key)):
         for j in range(len(key)):
-            key[i][j] = temp[len(key)-j-1][i]
-    
-    return key
-
-def insert(answer,m,n,bd,cnt,key,x,y):
-    if answer == True:
-        return
-    rs = 0
-    for i in range(m):
-        for j in range(m):
             if m-2 < i+x < n+m-1 and m-2 < j+y < n+m-1:
                 if bd[i+x][j+y] == 0:
                     if key[i][j] == 1:
@@ -27,32 +26,33 @@ def insert(answer,m,n,bd,cnt,key,x,y):
                     else:
                         return 0
     if rs == cnt:
-        answer = True
         return True
 
 def solution(key, lock):
-    cnt = 0
-    for i in range(len(lock)):
-        for j in range(len(lock)):
-            if lock[i][j] == 0:
-                cnt += 1
     n = len(lock)
     m = len(key)
+    near = [[0,1],[1,0]]
+    temp = [i[:] for i in key]
+    r1 = rotate(key)
+    r2 = rotate(r1)
+    r3 = rotate(r2)
+    q = [key,r1,r2,r3]
     bd = [[0]*(n+(2*(m-1))) for i in range(n+(2*(m-1)))]
-    r1 = [i[:] for i in rotate(key)]
-    r2 = [i[:] for i in rotate(key)]
-    r3 = [i[:] for i in rotate(key)]
-    r4 = [i[:] for i in rotate(key)]
-    q = [r1,r2,r3,r4]
-    
-    answer = False
+    cnt = 0
+    for i in range(n):
+        for j in range(n):
+            if bd[i][j] == 0:
+                cnt += 1
+    for i in range(len(lock)):
+        for j in range(len(lock)):
+            bd[m+i-1][m+j-1] = lock[i][j]
+            
     for k in q:
-        if answer == True:
-            break
         for x in range(n+m-2):
             for y in range(n+m-2):
-                if insert(answer,m,n,bd,cnt,k,x,y) == True:
+                if insert(bd,key,lock,cnt,x,y) == True:
                     return True
+    
     return False
 
 print(solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]]))
